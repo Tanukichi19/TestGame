52aa1184ca7ae82ae6718b4ec7591fd85702c287
Tanukichi19 <taisei_19n1100251@nnn.ed.jp>
2021-09-19 17:48:43 +0900

    delete

--- Build/WebGL.framework.js
+++ Build/WebGL.framework.js
@@ -1284,7 +1284,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 3446704;
+STATICTOP = STATIC_BASE + 3446640;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AIScriptingClasses_cpp();
@@ -3310,7 +3310,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 3446704;
+var STATIC_BUMP = 3446640;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
